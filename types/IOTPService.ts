7f63be324af6f503f1ae04d39ce1baaf4f9bcb7d
DevJin27 <devjindal2317@gmail.com>
2026-04-13 17:12:24 +0530

    chore: bootstrap npm workspaces monorepo

--- types/IOTPService.ts
+++
@@ -1,6 +0,0 @@
-export interface IOTPService {
-  generate(): string;
-  store(phone: string, otp: string): Promise<void>;
-  verify(phone: string, otp: string): Promise<boolean>;
-  invalidate(phone: string): Promise<void>;
-}
